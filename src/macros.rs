7981f46f26e21da66e45ea9a37b174c6eef039a7
Las <las@protonmail.ch>
2019-02-09 13:18:44 +0100

    Don't assume the name of the Steam API library file

--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,5 @@
 macro_rules! steam_extern {
 	($($t:tt)*) => {
-		#[cfg_attr(unix,    link(name = "steam_api"))]
-		#[cfg_attr(windows, link(name = "steam_api64"))]
 		#[no_mangle]
 		extern "C" {
 			$($t)*
